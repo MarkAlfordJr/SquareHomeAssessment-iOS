7fa01dddfbdafa7ad75ef5db0933b3661e6bc4a1
Mark Alford Jr <markalfordjr@gmail.com>
2022-04-05 09:16:06 -0400

    Initial Commit

---
+++ SquareTakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareTakeHome
+//
+//  Created by Mark Alford on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
